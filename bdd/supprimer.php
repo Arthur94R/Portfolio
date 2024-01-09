8781470fa84a345e5ea77f863354830b2c8c30a6
Arthur94R <81250883+Arthur94R@users.noreply.github.com>
2024-01-09 14:23:28 +0100

    Delete bdd directory

--- bdd/supprimer.php
+++
@@ -1,8 +0,0 @@
-<?php
-    include "head.php";
-    $id=$_GET["id"];
-    echo $id;
-    $req="delete from produit whre id=$id";
-    $resultat=$bdd->query($req);
-    header("location:index.php");
-?>
